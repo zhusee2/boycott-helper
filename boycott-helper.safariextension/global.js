368447a18173c7f78df9ebf79c2233f019ffc40b
Zhusee <zhusee2@Mac-mini.local>
2013-09-14 23:52:37 +0800

    Add blacklist rule interpreter

--- boycott-helper.safariextension/global.js
+++ boycott-helper.safariextension/global.js
@@ -1,10 +1,38 @@
-boycottList = [
-  new RegExp("https?\\:\\/\\/www.google.com.tw")
+blackList = [
+  "*.google.com.tw",
+  "www.appledaily.com.tw/realtimenews/article/politics/*"
 ]
 
+// Black List Rules:
+// 1. "*.bad.domain.com"
+// 2. "bad-website.xxx/horrible/*"
+// 3. "example.com"
+// 4. "test.*.bad.domain.xxx/test/*/another/*"
+
+function interpretBlackListRule(rule) {
+  regExpRule = rule.replace(/^https?\:\/\//, ""); // Remove protocol
+  regExpRule = regExpRule.replace(/\./g, "\\.");  // Escape dot (.)
+  regExpRule = regExpRule.replace(/\//g, "\\/");  // Escape forward slash (/)
+
+  // Replace "*" in domain part
+  regExpRule = regExpRule.replace(/^\*/, "[^\\.\\/]+");
+  regExpRule = regExpRule.replace(/\.\*\./g, "\\.[^\\.\\/]+\\.");
+  regExpRule = regExpRule.replace(/(^[^\/]+\.)\*/, "$1[^\\.\\/]+");
+
+  regExpRule = regExpRule.replace(/\/\*/g, "/[^\/]*") // Replace "*" in path part
+
+  regExpRule = "https?\\:\\/\\/" + regExpRule;    // Prepend protocol
+
+  return regExpRule;
+}
+
 function beforeNavHandler(event) {
-  for (var i = 0; i < boycottList.length; i++) {
-    if (event.url.match(boycottList[i]) && !confirm("您已經設定要抵制本網站。確定要繼續嗎？")) {
+  console.log("Before heading to:", event.url);
+
+  for (var i = 0; i < blackList.length; i++) {
+    var regExpRule = interpretBlackListRule(blackList[i]);
+
+    if (event.url.match(regExpRule) && !confirm("您已經設定要抵制本網站。確定要繼續嗎？")) {
       event.preventDefault();
     }
   }
